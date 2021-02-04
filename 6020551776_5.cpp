60a858e7da80adf260a2deed4a4f99542e1766ce
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-02-04 18:05:16 +0700

    แก้ไขยังไม่เส็จ

--- 6020551776_5.cpp
+++ 6020551776_5.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int a,i,n,answer=0,num=0;
+    int a,i,n,answer=0,num[i];
     scanf("%d",&n);
     for(i=0; i<n; i++)
     {
         scanf("%d",&a);
         num=i;
+        if(num[i]>answer)
+        {
+        	n=i;
+        	max=num[i];
+		}
         if(a>answer)
         {
             answer=a;
